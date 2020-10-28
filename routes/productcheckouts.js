df469ab2f63fae71bac57ef334264fa9473fcf4a
kishan movaliya <km.globaliasoft@gmail.com>
2020-10-28 18:07:49 +0530

    work on making api of checkout data

--- routes/productcheckouts.js
+++ routes/productcheckouts.js
@@ -2,12 +2,48 @@ const express = require("express");
 const verify = require("../middleware/verifyToken");
 const router = express.Router();
 const Productcheckout = require("../models/productcheckouts");
+const Addtocart = require("../models/addtocarts");
 
 //-----add product for checkout-----
 router.post("/finalcheckout", verify, async (req, res) => {
   const userId = req.user._id;
+  const { cartDetails } = req.body;
+  const checkoutDetails = req.body.checkoutDetails;
+  // console.log("checkoutDetails:", checkoutDetails);
 
-  return res.json({ status: 200, msg: req.user._id });
+  try {
+    const dataExist = await Productcheckout.find({ userId: userId, status: 0 });
+    if (dataExist.length > 0) {
+      return res.json({ status: 200, data: "your ordered already booked." });
+    } else {
+      console.log("dataExist");
+      const checkoutData = new Productcheckout({
+        addtocartId: cartDetails,
+        userId: userId,
+        name: checkoutDetails.name,
+        address: checkoutDetails.address,
+        contact: checkoutDetails.contact,
+      });
+      const data = await checkoutData.save();
+
+      const upStatus = await Addtocart.find({
+        _id: cartDetails,
+        userId: userId,
+      });
+
+      if (upStatus) {
+        upStatus.map(async (upstatus) => {
+          const st = await Addtocart.findById(upstatus._id);
+          console.log(st);
+          st.status = 1;
+          await st.save();
+        });
+      }
+      return res.json({ status: 200, data: data });
+    }
+  } catch (error) {
+    return res.json({ status: 400, msg: error });
+  }
 });
 
 module.exports = router;
