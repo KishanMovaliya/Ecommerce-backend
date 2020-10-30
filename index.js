3a538087d9e6cac9f4f854f1aff7d7e8d71eba95
kishan movaliya <km.globaliasoft@gmail.com>
2020-10-30 15:59:35 +0530

    resolved the headers

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 //for set image publically
 app.use("/uploads", express.static("uploads"));
 
