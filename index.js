06f757a9908bbd81e3caca6a7d702105d958c196
kishan movaliya <km.globaliasoft@gmail.com>
2020-10-30 16:10:01 +0530

    resolved the headers

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
   );
   next();
 });
